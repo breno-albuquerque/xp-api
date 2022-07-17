0d6d0ec3702bd00c1df039e32b61bcbabda25545
Breno Albuquerque <brenoralbu@hotmail.com>
2022-07-17 16:30:57 -0300

    feat(routes): Adiciona router para exportar rotas

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,8 +1,11 @@
 import express from 'express';
+import router from './routes';
 
 const app = express();
 
 app.use(express.json());
+
+app.use(router);
 
 app.get('/', (_req, res) => res.send('Working'));
 
