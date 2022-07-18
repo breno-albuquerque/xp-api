32a20f3dc400a14e243ee60b58443720dca336df
Breno Albuquerque <brenoralbu@hotmail.com>
2022-07-18 16:19:33 -0300

    style(tests): Melhora sintaxe, identação e espaçamento do Auth.controller test

--- server/src/tests/controllers/Auth.controller.spec.ts
+++ server/src/tests/controllers/Auth.controller.spec.ts
@@ -5,55 +5,47 @@ import AuthController from '../../controllers/Auth.controller';
 import AuthService from '../../services/Auth.service';
 import sinonChai from 'sinon-chai';
 
-const newContaMock = {
-  nome: 'Conta Mock',
-  cpf: '11111111111',
-  email: 'conta@mock.com',
-  senha: '123456'
-}
-
-const tokenMock = 'jwt-mock';
 chai.should();
 chai.use(sinonChai);
 
+const tokenMock = 'jwt-mock';
 
 describe('Testa funções do AuthController', () => {
   describe('Função create', async () => {
-    let stub = sinon.stub(AuthService, 'create').resolves(tokenMock);
-
+    let request = {};
     let response: Partial<Response> = {
       json: sinon.stub().returns(tokenMock),
       status: sinon.stub().returns(201)
     };
-    let request = {};
     let next = () => {};
-  
-    after(async () => {
-      stub.restore()
-    });
+
+    let stub = sinon.stub(AuthService, 'create').resolves(tokenMock);
+
+    after(async () => { stub.restore() });
 
     it('É chamado o status da Response com código 201', async () => {
-      await AuthController.create(request as Request, response as Response, next as NextFunction);
-      expect(response.status).calledWith(201)  
+      await AuthController
+        .create(request as Request, response as Response, next as NextFunction);
+      expect(response.status).to.have.been.calledWith(201);
     });
   });
 
   describe('Função login', async () => {
     let stub = sinon.stub(AuthService, 'login').resolves(tokenMock);
+
+    let request = {};
     let response: Partial<Response> = {
       json: sinon.stub().returns(tokenMock),
       status: sinon.stub().returns(200)
     };
-    let request = {};
     let next = () => {};
 
-    after(async () => {
-      stub.restore()
-    });
+    after(async () => { stub.restore() });
 
     it('É chamado o status da Response com código 200', async () => {
-      await AuthController.login(request as Request, response as Response, next as NextFunction);
-      expect(response.status).calledWith(200);
+      await AuthController
+        .login(request as Request, response as Response, next as NextFunction);
+      expect(response.status).to.have.been.calledWith(200);
     });
   });
 });
