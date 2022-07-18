9256afa194638775756641b7feaf10afd6bbcaff
Breno Albuquerque <brenoralbu@hotmail.com>
2022-07-18 19:27:51 -0300

    refactor(models): Renomeia classes para SaqueModel e DepositoModel

--- server/src/models/Deposito.model.ts
+++ server/src/models/Deposito.model.ts
@@ -1,11 +1,11 @@
 import { ResultSetHeader } from "mysql2";
 import IConnection from "../interfaces/connection/IConnection";
 
-class Deposito {
+class DepositoModel {
   public static async create(conn: IConnection, values: number[]): Promise<number> {
     const result = await conn.run(conn.queries.createDeposito, values) as ResultSetHeader;
     return result.insertId;
   }
 }
 
-export default Deposito;
+export default DepositoModel;
--- server/src/models/Saque.model.ts
+++ server/src/models/Saque.model.ts
@@ -1,11 +1,11 @@
 import { ResultSetHeader } from "mysql2";
 import IConnection from "../interfaces/connection/IConnection";
 
-class Saque {
+class SaqueModel {
   public static async create(conn: IConnection, values: number[]): Promise<number> {
     const result = await conn.run(conn.queries.createSaque, values) as ResultSetHeader;
     return result.insertId;
   }
 }
 
-export default Saque;
+export default SaqueModel;
