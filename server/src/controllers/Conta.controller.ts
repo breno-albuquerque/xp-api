9b37d3dea677dc20da667f86aa7a93b3069fff00
Breno Albuquerque <brenoralbu@hotmail.com>
2022-07-17 22:14:13 -0300

    feat(utils): Adiciona enum com status http

--- server/src/controllers/Conta.controller.ts
+++ server/src/controllers/Conta.controller.ts
@@ -1,10 +1,11 @@
 import { NextFunction, Request, Response } from 'express';
 import ContaService from '../services/Conta.service';
+import HttpStatus from '../utils/http.status';
 
 const create = async (req: Request, res: Response, next: NextFunction) => {
  try {
     await ContaService.create(req.body);
-    return res.status(201).end();
+    return res.status(HttpStatus.CREATED).end();
   } catch (error) {
     next(error);
   }
@@ -14,7 +15,7 @@ const getById = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const { contaId } = req.params;
     const conta = await ContaService.getById(parseInt(contaId));
-    return res.status(200).json(conta);
+    return res.status(HttpStatus.OK).json(conta);
   } catch (error) {
     next(error);
   }
--- server/src/services/Conta.service.ts
+++ server/src/services/Conta.service.ts
@@ -3,12 +3,13 @@ import IConta from "../interfaces/conta/IConta";
 import INewConta from "../interfaces/conta/INewConta";
 import ContaModel from "../models/Conta.model"
 import HttpException from "../utils/http.exception";
+import HttpStatus from "../utils/http.status";
 
 class ContaService {
   public static async create(conta: INewConta): Promise<void>  {
     //  Verifica se email ja está registrado:
     const isRegistered = await ContaModel.getByEmail(MyConnection.queries.getContaByEmail, conta.email);
-    if (isRegistered) throw new HttpException(409, 'Esse email não está disponível');
+    if (isRegistered) throw new HttpException(HttpStatus.CONFLICT, 'Esse email não está disponível');
 
     await ContaModel.create(MyConnection.queries.createConta, conta);
   }
