66daba00a46e9cb039f42e73e2afa14aeb83ecd7
Breno Albuquerque <brenoralbu@hotmail.com>
2022-07-18 00:09:28 -0300

    feat: Senha do usuário agora é salva no banco de dados em forma de hash

--- server/src/routes/Conta.route.ts
+++ server/src/routes/Conta.route.ts
@@ -4,6 +4,7 @@ import ContaController from '../controllers/Conta.controller';
 const router = Router();
 
 router.get('/:contaId', ContaController.getById);
+
 router.post('/register', ContaController.create);
 router.post('/login', ContaController.login);
 
--- server/src/services/Conta.service.ts
+++ server/src/services/Conta.service.ts
@@ -5,31 +5,34 @@ import ContaModel from "../models/Conta.model"
 import HttpException from "../utils/http.exception";
 import HttpStatus from "../utils/http.status";
 import { generateToken } from "../utils/jwt.token";
+import bcrypt from 'bcrypt';
 
 class ContaService {
-  public static async create(conta: INewConta): Promise<string | undefined> {
-    //  Verifica se email ja está registrado:
+  private static async verifyEmail(conta: INewConta) {
     const isRegistered = await ContaModel.getByEmail(MyConnection.queries.getContaByEmail, conta.email);
     if (isRegistered) throw new HttpException(HttpStatus.CONFLICT, 'Esse email não está disponível');
+  }
+
+  public static async create(conta: INewConta): Promise<string | undefined> {
+    //  Verifica se email ja está registrado:
+    await this.verifyEmail(conta);
 
     //  Cria conta:
-    const insertId = await ContaModel.create(MyConnection.queries.createConta, conta);
+    const { email, password } = conta;
+    const hash = await bcrypt.hash(password, 5);
+    const insertId = await ContaModel.create(MyConnection.queries.createConta, { email, password: hash });
 
     // Gera token:
     const token = generateToken({ id: insertId, email: conta.email });
     return token;
   }
 
-  public static async login(conta: INewConta) {
+  public static async login(conta: INewConta): Promise<string | undefined> {
       const dadosConta = await ContaModel.getByEmail(MyConnection.queries.getContaByEmail, conta.email);
+      if (!dadosConta) throw new HttpException(HttpStatus.UNAUTHORIZED, 'Email ou senha inválidos');
 
-      //  Valida dados:
-      if (!dadosConta) {
-        throw new HttpException(HttpStatus.UNAUTHORIZED, 'Email ou senha inválidos');
-      }
-      if (dadosConta.password !== conta.password) {
-        throw new HttpException(HttpStatus.UNAUTHORIZED, 'Email ou senha inválidos');
-      }
+      const isMatch = await bcrypt.compare(conta.password, dadosConta.password);
+      if (!isMatch) throw new HttpException(HttpStatus.UNAUTHORIZED, 'Email ou senha inválidos');
 
       // Gera token:
       const token = generateToken({ id: dadosConta.id, email: conta.email });
