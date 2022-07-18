fda29554eae08742c2524e06da5fde97305f38d3
Breno Albuquerque <brenoralbu@hotmail.com>
2022-07-17 22:23:06 -0300

    feat(utils): Adiciona funções para gerar e verificar token

---
+++ server/src/utils/jwt.token.ts
@@ -0,0 +1,29 @@
+import { JwtPayload, sign, SignOptions, verify } from 'jsonwebtoken';
+import IConta from '../interfaces/conta/IConta';
+import HttpException from './http.exception';
+import HttpStatus from './http.status';
+
+const SECRET = process.env.JWT_SECRET || 'secret';
+
+const jwtConfig: SignOptions = {
+  algorithm: 'HS256',
+};
+
+const generateToken = (payload: Omit<IConta, 'password, saldo'>): string => {
+  const token: string = sign(payload, SECRET, jwtConfig);
+  return token;
+};
+
+const verifyToken = (token: string) => {
+  try {
+    const decrypted = verify(token, SECRET, jwtConfig);
+    return decrypted;
+  } catch (err) {
+    throw new HttpException(HttpStatus.BAD_REQUEST, 'Token inválido');
+  }
+};
+
+export default {
+  generateToken,
+  verifyToken
+}
