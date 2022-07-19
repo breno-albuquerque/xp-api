64271976e96c8f914fde53ec033e61d0b9da26ce
Breno Albuquerque <brenoralbu@hotmail.com>
2022-07-19 09:27:48 -0300

    refactor(Ativo.service): Separa formatação do Ativo em método específico

--- server/src/services/Ativo.service.ts
+++ server/src/services/Ativo.service.ts
@@ -2,18 +2,15 @@ import MyConnection from "../database/MyConnection";
 import AtivoModel from "../models/Ativo.model";
 import HttpException from "../utils/http.exception";
 import HttpStatus from "../utils/http.status";
+import IAtivo from "../interfaces/ativo/IAtivo";
 
 class AtivoService {
   public static async getById(assetId: number) {
       const asset = await AtivoModel.getById(MyConnection, assetId);
       if (!asset) throw new HttpException(HttpStatus.NOT_FOUND, 'Ativo não encontrado');
-      const latestValue = await AtivoService.getValue(asset.simbolo);
 
-      const fullAsset = {
-        CodAtivo: asset.id,
-        QtdeAtivo: asset.quantidade,
-        Valor: latestValue
-      }
+      const latestValue = await this.getValue(asset.simbolo);
+      const fullAsset = this.formatAsset(asset, latestValue);
 
       return fullAsset;
   }
@@ -22,6 +19,14 @@ class AtivoService {
     const assetLatestResponse = await fetch(`https://www.okanebox.com.br/api/acoes/ultima/${assertSymbol}`);
     const assetLatestData = await assetLatestResponse.json();
     return assetLatestData.PREULT;
+  }
+
+  private static formatAsset(asset: IAtivo, latestValue: number) {
+    return {
+      CodAtivo: asset.id,
+      QtdeAtivo: asset.quantidade,
+      Valor: latestValue
+    }
   }
 }
 
