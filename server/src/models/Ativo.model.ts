8bd760b94801c22fde667deb12cb1b1bc6cb0464
Breno Albuquerque <brenoralbu@hotmail.com>
2022-07-19 15:44:51 -0300

    feat(Ativo.model): Adiciona método update

--- server/src/models/Ativo.model.ts
+++ server/src/models/Ativo.model.ts
@@ -1,4 +1,4 @@
-import { RowDataPacket } from "mysql2";
+import { ResultSetHeader, RowDataPacket } from "mysql2";
 import IAtivo from "../interfaces/ativo/IAtivo";
 import IConnection from "../interfaces/connection/IConnection";
 
@@ -6,6 +6,11 @@ class AtivoModel {
   public static async getById(conn: IConnection, value: number): Promise<IAtivo> {
     const [result] = await conn.run(conn.queries.getAtivoById, [value]) as RowDataPacket[];
     return result as IAtivo;
+  }
+
+  public static async update(conn: IConnection, value: number): Promise<number> {
+    const result = await conn.run(conn.queries.updateAtivo, [value]) as ResultSetHeader;
+    return result.affectedRows;
   }
 }
 
