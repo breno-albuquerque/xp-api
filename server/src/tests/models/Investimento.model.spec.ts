61222c2e24dc6346d07731a59251febba9d1be42
Breno Albuquerque <brenoralbu@hotmail.com>
2022-07-19 13:52:09 -0300

    tests(Investimento.model): Adiciona teste unitário para método getOne

--- server/src/tests/models/Investimento.model.spec.ts
+++ server/src/tests/models/Investimento.model.spec.ts
@@ -3,6 +3,13 @@ import sinon from 'sinon';
 import MyConnection from "../../database/MyConnection";
 import InvestimentoModel from "../../models/Investimento.model";
 
+
+const investimentoMock = {
+  contaId: 1,
+  ativoId: 1,
+  quantidade: 10
+}
+
 const conn = MyConnection;
 
 describe('Testa métodos da classe InvestimentoModel em Investimento.model.ts', () => {
@@ -18,7 +25,7 @@ describe('Testa métodos da classe InvestimentoModel em Investimento.model.ts', 
         stub.restore();
       })
 
-      it('Deve retornar o ativo correspondente', async () => {
+      it('Deve retornar o número do id do investimento criado', async () => {
         const result = await InvestimentoModel.create(MyConnection, 1, 1, 10);
 
         expect(result).to.be.a('number');
@@ -39,7 +46,7 @@ describe('Testa métodos da classe InvestimentoModel em Investimento.model.ts', 
         stub.restore();
       })
 
-      it('Deve retornar o ativo correspondente', async () => {
+      it('Deve retornar o número de linhas afetadas', async () => {
         const result = await InvestimentoModel.update(MyConnection, 1, 1, 10);
 
         expect(result).to.be.a('number');
@@ -48,7 +55,7 @@ describe('Testa métodos da classe InvestimentoModel em Investimento.model.ts', 
     });
   });
 
-  describe('Método update', () => {
+  describe('Método delete', () => {
     describe('Quando passa valores válidos', () => {
       let stub: sinon.SinonStub;
 
@@ -60,11 +67,35 @@ describe('Testa métodos da classe InvestimentoModel em Investimento.model.ts', 
         stub.restore();
       })
 
-      it('Deve retornar o ativo correspondente', async () => {
+      it('Deve retornar o número de linhas afetadas', async () => {
         const result = await InvestimentoModel.delete(MyConnection, 1, 1);
 
         expect(result).to.be.a('number');
         expect(result).to.equal(1);
+      });
+    });
+  });
+
+  describe('Método getOne', () => {
+    describe('Quando passa valores válidos', () => {
+      let stub: sinon.SinonStub;
+
+      beforeEach(async () => {
+        stub = sinon.stub(conn, 'run').resolves([investimentoMock]);
+      });
+
+      afterEach(async () => {
+        stub.restore();
+      })
+
+      it('Deve retornar o investimento correspondente', async () => {
+        const result = await InvestimentoModel.getOne(MyConnection, 1, 1);
+
+        expect(result).to.be.an('object');
+        expect(result).to.contain.all.keys('contaId', 'ativoId', 'quantidade');
+        expect(result.contaId).to.equal(1);
+        expect(result.ativoId).to.equal(1);
+        expect(result.quantidade).to.equal(10);
       });
     });
   });
