5082a26d7af176e9652836090c7c46ef8b8537e3
Breno Albuquerque <brenoralbu@hotmail.com>
2022-07-20 21:33:16 -0300

    refactor(/tests/controllers): Melhora sintaxe dos testes de Ativo.controller

--- server/src/tests/controllers/Ativo.controller.spec.ts
+++ server/src/tests/controllers/Ativo.controller.spec.ts
@@ -14,22 +14,36 @@ const fullAssetMock = {
   Valor: 30.00,
 };
 
+//  Referência: Solução para criar spy de Request e Response com Typescript
+
+//  https://jonathanwatsonwebdevelopment.medium.com/how-to-unit-test-express-controllers-with-mocha-and-chai-5cb425c5c7db
+const statusJsonSpy = sinon.spy();
+
+const response: Partial<Response> = {
+  json: sinon.spy(),
+  status: sinon.stub().returns({ json: statusJsonSpy }),
+};
+
+const request: Partial<Request> = {
+  params: { param: 'temp' },
+};
+
+const next = () => {};
+
 describe('Testa funções do AtivoController', () => {
-  describe('Função getById', () => {
-    const request: Partial<Request> = {
-      params: { codAtivo: '1' },
-    };
-    const response: Partial<Response> = {
-      json: sinon.stub().returns(fullAssetMock),
-      status: sinon.stub().returns(200),
-    };
-    const next = () => {};
+  let stub: sinon.SinonStub;
 
-    const stub = sinon.stub(AtivoService, 'getById').resolves(fullAssetMock);
+  after(() => {
+    stub.restore();
+  });
 
-    after(async () => { stub.restore(); });
-
-    it('É chamado o status da Response com código 200', async () => {
+  context('Função getById', () => {
+    it('É chamado o status da Response com código 200 e o Ativo correspondente no json', async () => {
+      stub = sinon
+        .stub(AtivoService, 'getById')
+        .resolves(fullAssetMock);
+      request.params = { codAtivo: '1' };
+  
       await AtivoController
         .getById(request as Request, response as Response, next as NextFunction);
 
