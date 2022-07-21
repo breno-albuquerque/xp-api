394829c6b880854e37193e3b675dbb2ada5f1395
Breno Albuquerque <brenoralbu@hotmail.com>
2022-07-21 20:49:39 -0300

    feat(app.ts): Adiciona express-async-errors para garantir captura de erros assíncronos

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import errorMiddleware from './middlewares/error.middleware';
 import router from './routes';
+import 'express-async-errors';
 
 const app = express();
 
@@ -8,7 +9,5 @@ app.use(express.json());
 
 app.use(router);
 app.use(errorMiddleware);
-
-app.get('/', (_req, res) => res.send('Working'));
 
 export default app;
