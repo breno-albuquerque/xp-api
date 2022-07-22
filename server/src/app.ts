5a07eb00b62c457c489bfc0111527a8ffde2252e
Breno Albuquerque <brenoralbu@hotmail.com>
2022-07-22 16:35:10 -0300

    docs: Inicializa swagger e adiciona tags usando JSDoc

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,11 +1,18 @@
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
 import express from 'express';
 import errorMiddleware from './middlewares/error.middleware';
 import router from './routes';
 import 'express-async-errors';
 
+import swaggerConfig from './docs/swagger.config';
+
 const app = express();
 
 app.use(express.json());
+
+const swaggerDoc = swaggerJSDoc(swaggerConfig);
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
 app.use(router);
 app.use(errorMiddleware);
--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -7,8 +7,37 @@ import InvestimentoRouter from './Investimento.route';
 const router = Router();
 
 router.use('/auth', ClienteRouter);
+/**
+ * @swagger
+ *  tags:
+ *      name: Auth
+ *      description: Endpoints de autorização
+ */
 router.use('/conta', ContaRouter);
+/**
+ * @swagger
+ *  tags:
+ *      name: Conta
+ *      description: Endpoints de operações da conta
+ */
+
+
 router.use('/ativos', AtivoRouter);
+/**
+ * @swagger
+ *  tags:
+ *      name: Ativos
+ *      description: Endpoints para buscar Ativos
+ */
+
+
+
 router.use('/investimentos', InvestimentoRouter);
+/**
+ * @swagger
+ *  tags:
+ *      name: Investimentos
+ *      description: Endpoints para realizar investimentos
+ */
 
 export default router;
