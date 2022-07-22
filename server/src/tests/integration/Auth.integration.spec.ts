435648d3df5f1d0854e34de7cf16657b8f2b53ae
Breno Albuquerque <brenoralbu@hotmail.com>
2022-07-22 09:49:50 -0300

    tests(integration): Adiciona testes de integração para rota /auth

---
+++ server/src/tests/integration/Auth.integration.spec.ts
@@ -0,0 +1,77 @@
+/* eslint-disable mocha/max-top-level-suites */
+import chai, { expect } from 'chai';
+import chaiHttp from 'chai-http';
+import bcrypt from 'bcrypt';
+import sinon from 'sinon';
+import server from '../../app';
+import ContaModel from '../../models/Conta.model';
+import { contaMock, newContaMock } from '../mocks/conta.mock.spec';
+import jwt from '../../utils/jwt.token';
+
+chai.use(chaiHttp);
+
+type Methods = 'create' | 'getByEmail' | 'getById' | 'update';
+
+const createModelStub = (resolveValue: any, method: Methods): sinon.SinonStub => (
+  sinon.stub(ContaModel, method).resolves(resolveValue)
+);
+
+const token = jwt.generateToken({ Id: 1, Nome: 'Conta Mock' });
+
+let response: any;
+let stub1: sinon.SinonStub;
+let stub2: sinon.SinonStub;
+
+describe('Integração /auth/registrar - register', () => {
+  afterEach(async () => {
+    stub1.restore();
+    stub2.restore();
+  });
+
+  beforeEach(async () => {
+    stub1 = createModelStub(undefined, 'getByEmail');
+    stub2 = createModelStub(1, 'create');
+
+    response = await chai
+      .request(server)
+      .post('/auth/registrar')
+      .send(newContaMock);
+  });
+
+  it('Deve retornar código status 201', () => {
+    expect(response).to.have.status(201);
+  });
+
+  it('Deve retornar o JWT correto', () => {
+    expect(response.body).to.have.key('token');
+    expect(response.body.token).to.be.a('string');
+    expect(response.body.token).to.equal(token);
+  });
+});
+
+describe('Integração /auth/entrar - login', () => {
+  afterEach(async () => {
+    stub1.restore();
+    stub2.restore();
+  });
+
+  beforeEach(async () => {
+    stub1 = createModelStub({ ...contaMock, Senha: await bcrypt.hash('123456', 5) }, 'getByEmail');
+    stub2 = createModelStub(1, 'create');
+
+    response = await chai
+      .request(server)
+      .post('/auth/entrar')
+      .send({ Email: 'conta@mock.com', Senha: '123456' });
+  });
+
+  it('Deve retornar código status 201', () => {
+    expect(response).to.have.status(200);
+  });
+
+  it('Deve retornar o JWT correto', () => {
+    expect(response.body).to.have.key('token');
+    expect(response.body.token).to.be.a('string');
+    expect(response.body.token).to.equal(token);
+  });
+});
