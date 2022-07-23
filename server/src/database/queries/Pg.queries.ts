72b5e6f8be1904d85411b1ff8abdc5ee412c2828
Breno Albuquerque <brenoralbu@hotmail.com>
2022-07-23 13:15:20 -0300

    fix(database): Corrige mais algumas queries do postgres

--- server/src/database/queries/Pg.queries.ts
+++ server/src/database/queries/Pg.queries.ts
@@ -4,7 +4,7 @@ export const qConta: IContaQueries = {
   create: 'INSERT INTO "Contas" ("Nome", "Cpf", "Email", "Senha") VALUES ($1,$2,$3,$4)',
   getById: 'SELECT "Id", "Nome", "Saldo" FROM "Contas" WHERE "Id" = $1',
   getByEmail: 'SELECT * FROM "Contas" WHERE "Email" = $1',
-  update: 'UPDATE "Contas" SET Saldo=$1 WHERE "Id"=$2',
+  update: 'UPDATE "Contas" SET "Saldo"=$1 WHERE "Id"=$2',
   delete: 'DELETE FROM "Contas" WHERE "Id"=$1',
 };
 
@@ -18,7 +18,7 @@ export const qSaque: ITransactionQueries = {
 
 export const qAtivo: IAtivoQueries = {
   getById: 'SELECT * FROM "Ativos" WHERE "Id" = $1',
-  update: 'UPDATE "Ativos" SET QtdeAtivo=$1 WHERE "Id"=$2',
+  update: 'UPDATE "Ativos" SET "QtdeAtivo"=$1 WHERE "Id"=$2',
   getAll: 'SELECT * FROM "Ativos"',
   getByClient: `SELECT a."Id", a."Simbolo", i."CodCliente", i."QtdeAtivo"
   FROM "Investimentos" AS i INNER JOIN "Ativos" AS a
@@ -27,7 +27,7 @@ export const qAtivo: IAtivoQueries = {
 
 export const qInvestimento: IInvestimentoQueries = {
   getOne: 'SELECT * FROM "Investimentos" WHERE "CodCliente"=$1 AND "CodAtivo"=$2',  
-  create: 'INSERT INTO "Investimentos" ("CodCliente", "CodAtivo", QtdeAtivo) VALUES ($1, $2, $3)',
-  update: 'UPDATE "Investimentos" SET QtdeAtivo=$1 WHERE "CodAtivo"=$2 AND "CodCliente"=$3',
+  create: 'INSERT INTO "Investimentos" ("CodCliente", "CodAtivo", "QtdeAtivo") VALUES ($1, $2, $3)',
+  update: 'UPDATE "Investimentos" SET "QtdeAtivo"=$1 WHERE "CodAtivo"=$2 AND "CodCliente"=$3',
   delete: 'DELETE FROM "Investimentos" WHERE "CodAtivo"=$1 AND "CodCliente"=$2',
 };
