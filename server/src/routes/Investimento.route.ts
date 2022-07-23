3b4fd1977e1ff19efcb96a583d82fad4793aa426
Breno Albuquerque <brenoralbu@hotmail.com>
2022-07-23 13:21:12 -0300

    feat(routes): Adiciona middleware investValidation

--- server/src/routes/Investimento.route.ts
+++ server/src/routes/Investimento.route.ts
@@ -2,20 +2,23 @@ import Router from 'express';
 import InvestimentoController from '../controllers/Investimento.controller';
 import { validateBodyId } from '../middlewares/cliente.middleware';
 import tokenValidation from '../middlewares/token.middleware';
+import investValidation from '../middlewares/investimento.middleware';
 
 const router = Router();
 
-router.post(
-  '/comprar',
+router.use(
   tokenValidation,
   validateBodyId,
+  investValidation,
+);
+
+router.post(
+  '/comprar',
   InvestimentoController.purchase,
 );
 
 router.post(
   '/vender', 
-  tokenValidation,
-  validateBodyId,
   InvestimentoController.sale,
 );
 
