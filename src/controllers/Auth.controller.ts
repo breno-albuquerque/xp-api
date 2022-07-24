a5f2a50d19c65355bae627c55a3b8e17c95d772e
Breno Albuquerque <brenoralbu@hotmail.com>
2022-07-24 11:03:18 -0300

    feat(controllers): Adiciona tipagem com generics em Requests

--- src/controllers/Auth.controller.ts
+++ src/controllers/Auth.controller.ts
@@ -1,8 +1,10 @@
+/* eslint-disable @typescript-eslint/ban-types */
 import { NextFunction, Request, Response } from 'express';
+import { INewConta } from '../interfaces/IConta';
 import AuthService from '../services/Auth.service';
 import HttpStatus from '../utils/http.status';
 
-const register = async (req: Request, res: Response, next: NextFunction) => {
+const register = async (req: Request<{}, {}, INewConta>, res: Response, next: NextFunction) => {
   try {
     const insertId = await AuthService.register(req.body);
     return res.status(HttpStatus.CREATED).json({ CodCliente: insertId });
--- src/controllers/Investimento.controller.ts
+++ src/controllers/Investimento.controller.ts
@@ -1,8 +1,10 @@
+/* eslint-disable @typescript-eslint/ban-types */
 import { NextFunction, Request, Response } from 'express';
+import IInvestimento from '../interfaces/IInvestimento';
 import InvestimentoService from '../services/Investimento.service';
 import HttpStatus from '../utils/http.status';
 
-const purchase = async (req: Request, res: Response, next: NextFunction) => {
+const purchase = async (req: Request<{}, {}, IInvestimento>, res: Response, next: NextFunction) => {
   try {
     await InvestimentoService.purchase(req.body);
     return res.status(HttpStatus.CREATED).end();
@@ -11,7 +13,7 @@ const purchase = async (req: Request, res: Response, next: NextFunction) => {
   }
 };
 
-const sale = async (req: Request, res: Response, next: NextFunction) => {
+const sale = async (req: Request<{}, {}, IInvestimento>, res: Response, next: NextFunction) => {
   try {
     await InvestimentoService.sale(req.body);
     return res.status(HttpStatus.CREATED).end();
